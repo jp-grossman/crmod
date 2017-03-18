a9a367e10d2377c61f4806bbcd1f244d4f1d6386
J.P. Grossman <jpg@alum.mit.edu>
2017-03-18 09:35:16 -0400

    Added utilities required for build and added READM

--- Make.bat
+++ Make.bat
@@ -1,10 +1,5 @@
-@cd ..
-@echo a > progs.dat
-@del progs.dat
-@cd source
-..\fastqcc\release\qccx /O2 %1 %2 %3 %4 %5 %6
-@cd ..
-crmake
-@REM @copy progs.dat e:
-@cd source
+@echo a > ..\progs.dat
+@del ..\progs.dat
+utils\qccx /O2 %1 %2 %3 %4 %5 %6
+utils\crmake ..
 
---
+++ README.txt
@@ -0,0 +1,4 @@
+Build with visual studio, or run make.bat from the command line.
+
+The build script expects progs.dat and userdefs.cfg to live in the parent directory.
+Obtain userdefs.cfg from the crmod release and edit as necessary.
